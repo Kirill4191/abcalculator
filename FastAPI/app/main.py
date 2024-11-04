0f5893e8f255def05aa4daf9994b3b4b8314e02a
Kirill4191 <Kir4191@gmail.com>
2024-11-04 19:13:56 +0500

    commit 6, MVP Backend

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
 
+# tbd separate controller and business logic layers (create seaprate py file for actual calculation)
+# tbd introduce model layer with pydantic
+# there won't be a persistence layer as there is no db involved
+
+app = FastAPI()
+
+@app.get("/calculator/sample-size") #routing layer
+async def calc(): #controller + business logic layers
+    return {"sample-size":10000}
+
+
+@app.get("/calculator/test-result")
+async def calc():
+    return {"p-val":0.1}
